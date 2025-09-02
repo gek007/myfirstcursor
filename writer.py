32f5a48d84551353ddee6edc64048b6ac1434e5a
kostya shilkrot <kostya.shilkrot@gmail.com>
2025-09-02 19:08:55 +0300

    added

--- writer.py
+++ writer.py
@@ -1,11 +1,27 @@
-def write_to_file(filename, data):
+def write_to_file(filename, text):
     with open(filename, 'w') as file:
-        file.write(data)
+        file.write(text)
 
 def read_from_file(filename):
     with open(filename, 'r') as file:
         return file.read()
 
+def append_to_file(filename, text):
+    with open(filename, 'a') as file:
+        file.write(text)
+
+def parser_input(text):
+    return text.split('\n')
+
+def parser_output(text):
+    return text.split('\n')
+
+
+
 if __name__ == "__main__":
-    write_to_file("test.txt", "Hello, World!")
+    write_to_file("test.txt", "Hello, World!\n")
+    append_to_file("test.txt", "Hello my name is Yosef\n")
+    input_text = input("Enter your text: ")
+    append_to_file("test.txt", input_text)
     print(read_from_file("test.txt"))
+
